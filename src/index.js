9ee3db0f5254503c0233f973eb1de9bc86fcc563
MauroSego <mauro.segovia.7@gmail.com>
2016-11-11 13:20:43 -0300

    Creada la modularización para el signup y el home en dos js diferentes

--- src/index.js
+++ src/index.js
@@ -1,56 +1,5 @@
 var page = require('page');
-var yo = require('yo-yo');
-var empty = require('empty-element');
-
-var main = document.getElementById('main-container');
-
-page('/', function (ctx, next) {
-  main.innerHTML = '<a href="/signup">Signup</a>' // diga home
-})
-
-page('/signup', function(ctx, next){
-  var main = document.getElementById('main-container');
-
-  var el = yo`<div class="container">
-      <div class="row">
-        <div class="col s10 push-s1">
-          <div class="row">
-            <div class="col m5 hide-on-small-only">
-              <img class="iphone" src="iphone.png" alt="">
-            </div>
-            <div class="col s12 m7">
-              <div class="row">
-                <div class="sign-up">
-                  <h1 class="platzigram">Photogram</h1>
-                  <form class="signup-form">
-                    <h2>Regístrate para ver fotos de tus amigos</h2>
-                    <div class="section">
-                      <a href="" class="btn btn-fb hide-on-small-only">Iniciar sesión con Facebook</a>
-                      <a href="" class="btn btn-fb hide-on-med-and-up">Iniciar sesión</a>
-                    </div>
-                    <div class="divider"></div>
-                    <div class="section">
-                      <input type="email" name="email" placeholder="Correo electrónico">
-                      <input type="text" name="name" placeholder="Nombre completo">
-                      <input type="text" name="username" placeholder="Nombre de usuario">
-                      <input type="password" name="password" placeholder="Contraseña">
-                      <button class="btn btn-signup waves-effect waves-light" type="submit">Registrate</button>
-                    </div>
-                  </form>
-                </div>
-              </div>
-              <div class="row">
-                <div class="login-box">
-                  ¿Ya tenes cuenta? <a href="/signin">Entrar</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>`
-
-  empty(main).appendChild(el);
-})
+require('./homepage');
+require('./signup');
 
 page();
